44011e82183bda4b456c1c67857cc0cd77b7913b
Mannar <manaraelkady@gmail.com>
2019-06-04 22:14:29 +0200

    added count for fingers detection

--- hand_gesture.py
+++ hand_gesture.py
@@ -2,6 +2,7 @@
 import cv2
 import imutils
 import numpy as np
+from sklearn.metrics import pairwise
 
 # global variables
 bg = None
@@ -44,6 +45,66 @@ def segment(image, threshold=25):
         # based on contour area, get the maximum contour which is the hand
         segmented = max(cnts, key=cv2.contourArea)
         return (thresholded, segmented)
+
+#-------------------------------------------------------------------------------
+# Function - To count the number of fingers in the segmented hand region
+#-------------------------------------------------------------------------------
+def count(thresholded, segmented):
+	# find the convex hull of the segmented hand region
+	chull = cv2.convexHull(segmented)
+
+	# find the most extreme points in the convex hull
+	extreme_top    = tuple(chull[chull[:, :, 1].argmin()][0])
+	extreme_bottom = tuple(chull[chull[:, :, 1].argmax()][0])
+	extreme_left   = tuple(chull[chull[:, :, 0].argmin()][0])
+	extreme_right  = tuple(chull[chull[:, :, 0].argmax()][0])
+
+	# find the center of the palm
+	cX = (extreme_left[0] + extreme_right[0]) // 2
+	cY = (extreme_top[1] + extreme_bottom[1]) // 2
+
+	# find the maximum euclidean distance between the center of the palm
+	# and the most extreme points of the convex hull
+	distance = pairwise.euclidean_distances([(cX, cY)], Y=[extreme_left, extreme_right, extreme_top, extreme_bottom])[0]
+	maximum_distance = distance[distance.argmax()]
+	
+	# calculate the radius of the circle with 80% of the max euclidean distance obtained
+	radius = int(0.8 * maximum_distance)
+	
+	# find the circumference of the circle
+	circumference = (2 * np.pi * radius)
+
+	# take out the circular region of interest which has 
+	# the palm and the fingers
+	circular_roi = np.zeros(thresholded.shape[:2], dtype="uint8")
+	
+	# draw the circular ROI
+	cv2.circle(circular_roi, (cX, cY), radius, 255, 1)
+	
+	# take bit-wise AND between thresholded hand using the circular ROI as the mask
+	# which gives the cuts obtained using mask on the thresholded hand image
+	circular_roi = cv2.bitwise_and(thresholded, thresholded, mask=circular_roi)
+
+	# compute the contours in the circular ROI
+	(cnts, _) = cv2.findContours(circular_roi.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+	# initalize the finger count
+	count = 0
+
+	# loop through the contours found
+	for c in cnts:
+		# compute the bounding box of the contour
+		(x, y, w, h) = cv2.boundingRect(c)
+
+		# increment the count of fingers only if -
+		# 1. The contour region is not the wrist (bottom area)
+		# 2. The number of points along the contour does not exceed
+		#     25% of the circumference of the circular ROI
+		if ((cY + (cY * 0.25)) > (y + h)) and ((circumference * 0.25) > c.shape[0]):
+			count += 1
+
+	return count
+
 
 #-------------------------------------------------------------------------------
 # Main function
@@ -107,6 +168,8 @@ if __name__ == "__main__":
 
                 # draw the segmented region and display the frame
                 cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                fingers = count(thresholded, segmented)
+                cv2.putText(clone, str(fingers), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
                 cv2.imshow("Thesholded", thresholded)
 
         # draw the segmented hand
